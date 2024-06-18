cafb3a3e50db7cc0396509fb594530a16cf99da4
Muhammad Reyan <reyankhalid0077@gmail.com>
2024-06-19 01:35:21 +0500

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -8,6 +8,8 @@ private:
     int attackPower;
 
 public:
+    
+// Default constructor that initialized according to current player
     Enemy(string playername) {
         if(playername == "Jon Snow"){
             name = "Night King";
@@ -27,12 +29,12 @@ public:
     int getHealth() const {
         return health;
     }
-
+   // Function to decrease health of enemy
     void takeDamage(int damage) {
         health -= damage;
         if (health < 0) health = 0;
     }
-
+    // Function to decrease health of player
     int attack() const {
         return attackPower;
     }
