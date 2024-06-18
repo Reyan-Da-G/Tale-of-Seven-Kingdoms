ef4229642581727d9384756cc537da7adf38269e
Muhammad Reyan <reyankhalid0077@gmail.com>
2024-06-19 01:30:56 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,14 +1,15 @@
 #include"game.h"
 
+// Function to hide cursor for smooth gameplay
 void hideCursor() {
-    cout << "\033[?25l"; 
+    cout << "\033[?25l";    // ANSI code for hiding the cursor
 }
 
 int Game :: currentscene = 0;  
 
 int main(){
     hideCursor();
-    Game* game = new Game;                     
+    Game* game = new Game;    // Dynamically creating object using class pointer                  
    
     delete game;
     game = NULL;              
